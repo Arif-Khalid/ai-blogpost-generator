b94881dd5edf1dd82a5739cb11569e003a1ef034
Arif <arifkhalid99@gmail.com>
2023-10-20 17:30:01 +0700

    Add and configure OpenAi API call

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,10 +1,26 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
 
 export default function NewPost(props) {
+  const [postContent, setPostContent] = useState("");
+
+  const handleClick = async () => {
+    const response = await fetch(`/api/generatePost`, {
+      method: "POST",
+    });
+
+    const json = await response.json();
+    console.log("RESULT: ", json);
+    setPostContent(json.post.postContent);
+  };
   return (
     <div>
       <h1>Hello New Post Page!</h1>
+      <button className="btn" onClick={handleClick}>
+        Generate Post
+      </button>
+      <div dangerouslySetInnerHTML={{ __html: postContent }}></div>
     </div>
   );
 }
