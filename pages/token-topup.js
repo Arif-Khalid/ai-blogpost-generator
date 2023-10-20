de565dd169065235b671b10dce03960cd20c5cb7
Arif <arifkhalid99@gmail.com>
2023-10-20 18:49:21 +0700

    Store available tokens in MongoDB

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,8 +1,16 @@
 import { AppLayout } from "../components/AppLayout";
 export default function TokenTopup() {
+  const handleClick = async () => {
+    await fetch(`/api/addTokens`, {
+      method: "POST",
+    });
+  };
   return (
     <div>
       <h1>Hello Token Topup Page!</h1>
+      <button className="btn" onClick={handleClick}>
+        Add tokens
+      </button>
     </div>
   );
 }
